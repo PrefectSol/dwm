aa39b24144bcd69889bc95b40c3f2de26d59320c
PrefectSol <prefectsol@mail.ru>
2025-01-03 22:52:03 +0500

    Add centered window name

--- config.h
+++ config.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int allowkill          = 1;        /* allow killing clients by default? */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "JetBrains Mono:size=12:antialias=true:hinting=true" };
+static const char dmenufont[]       = "JetBrains Mono:size=12:antialias=true:hinting=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -20,8 +20,8 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "∅", "∅", "∅", "∅", "∅" };
-static const char *alttags[] = { "[∅]", "[∅]", "[∅]", "[∅]", "[∅]"};
+static const char *tags[] = { "cmd", "web", "dev", "chat", "gen" };
+static const char *alttags[] = { "[cmd]", "[web]", "[dev]", "[chat]", "[gen]"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -61,8 +61,9 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *incvol[] = {"/usr/bin/amixer", "set", "Master", "5+", NULL};
-static const char *decvol[] = {"/usr/bin/amixer", "set", "Master", "5-", NULL};
+static const char *incvol[] = {"amixer", "set", "Master", "5+", NULL};
+static const char *decvol[] = {"amixer", "set", "Master", "5-", NULL};
+static const char *mutevol[] = {"amixer", "set", "Master", "toggle", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -99,8 +100,9 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ 0,				XF86XK_AudioLowerVolume,spawn,{.v = decvol} },	
-	{ 0,				XF86XK_AudioRaiseVolume,spawn,{.v = incvol} },
+    { MODKEY,             XK_F2,      spawn, {.v = decvol} }, 
+    { MODKEY,             XK_F3,      spawn, {.v = incvol} }, 
+    { MODKEY,             XK_F4,      spawn, {.v = mutevol} },
 };
 
 /* button definitions */
